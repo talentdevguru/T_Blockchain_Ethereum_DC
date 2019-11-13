a66b6c18fb8e1fc28ca5ee0709889e145d269a3f
talentdevguru <talent.dev.guru@gmail.com>
2019-11-13 22:31:07 +0900

    no message

---
+++ contracts/Notary.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+
+contract Notary {
+	struct Record {
+		uint mineTime;
+		uint blockNumber;
+	}
+
+	mapping(bytes32 => Record) private docHashes;
+
+  	constructor() public {
+  	}
+
+  	function addDocHash (bytes32 hash) public {
+  		Record memory newRecord = Record(now, block.number);
+  		docHashes[hash] = newRecord;
+  	}
+
+  	function findDocHash (bytes32 hash) public view returns(uint, uint) {
+  		return (docHashes[hash].mineTime, docHashes[hash].blockNumber);
+  	}
+}
